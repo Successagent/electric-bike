10fb1a35d668ad79bf835ae889e6a249297a8c68
successAgent <miesineagent@gmail.com>
2022-11-11 22:42:16 +0100

    updated

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,45 +5,59 @@ import cartLogo from '../../assets/cart.png'
 import profileLogo from '../../assets/profile.png'
 import heartLogo from '../../assets/heart.png'
 import { useState } from 'react'
+import MenuLogo from '../../assets/menu.svg'
+import MobileHeaderLogo from ""
 import { Link } from 'react-router-dom'
 
 const index = () => {
   return (
-    <header>
-      <div className="header-logo-con">
-        <img src={headerLogo} alt="header-logo" />
+    <>
+      <header>
+        <div className="header-logo-con">
+          <img src={headerLogo} alt="header-logo" />
+        </div>
+        <ul className="header-list">
+          <li>
+            <Link style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+          </li>
+          <li>
+            <Link style={{ color: 'white', textDecoration: 'none' }}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link style={{ color: 'white', textDecoration: 'none' }}>
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link style={{ color: 'white', textDecoration: 'none' }}>
+              Deals
+            </Link>
+          </li>
+          <li>
+            <Link style={{ color: 'white', textDecoration: 'none' }}>
+              Support
+            </Link>
+          </li>
+          <li>
+            <Link style={{ color: 'white', textDecoration: 'none' }}>Blog</Link>
+          </li>
+        </ul>
+        <ul className="header-icons-con">
+          <img src={searchLogo} alt="" />
+          <img src={heartLogo} alt="" />
+          <img src={profileLogo} alt="" />
+          <img src={cartLogo} alt="" />
+        </ul>
+      </header>
+      <div className="mobile-header">
+        <div className="menu">
+          <img src={MenuLogo} alt="" />
+
+        </div>
       </div>
-      <ul className="header-list">
-        <li>
-          <Link style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
-        </li>
-        <li>
-          <Link style={{ color: 'white', textDecoration: 'none' }}>About</Link>
-        </li>
-        <li>
-          <Link style={{ color: 'white', textDecoration: 'none' }}>
-            Products
-          </Link>
-        </li>
-        <li>
-          <Link style={{ color: 'white', textDecoration: 'none' }}>Deals</Link>
-        </li>
-        <li>
-          <Link style={{ color: 'white', textDecoration: 'none' }}>
-            Support
-          </Link>
-        </li>
-        <li>
-          <Link style={{ color: 'white', textDecoration: 'none' }}>Blog</Link>
-        </li>
-      </ul>
-      <ul className="header-icons-con">
-        <img src={searchLogo} alt="" />
-        <img src={heartLogo} alt="" />
-        <img src={profileLogo} alt="" />
-        <img src={cartLogo} alt="" />
-      </ul>
-    </header>
+    </>
   )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,8 +9,15 @@ import Vector from '../../assets/Vector.png'
 import MotorBike from '../../assets/tabler_motorbike.png'
 import Skate from '../../assets/icons8-skateboard.png'
 import Cooter from '../../assets/icons8-gyroscooter.png'
+import Global from '../../assets/global.png'
+import Message from '../../assets/message-question.png'
+import footerlogo from '../../assets/footerlogo.png'
+import Visa from '../../assets/image210.svg'
+import Master from '../../assets/image212.svg'
+import Security from '../../assets/security-safe.png'
 import Button from '../../Components/Button'
-import { CgFacebook } from 'react-icons/cg'
+import { CgFacebook, CgInstagram, CgTwitter } from 'react-icons/cg'
+import { CiTwitter } from 'react-icons/ci'
 import { AiOutlineGlobal } from 'react-icons/ai'
 
 const index = () => {
@@ -87,9 +94,93 @@ const index = () => {
         <div className="feature-card-two">
           <FeatureCard reverse="reverse" />
         </div>
-        <div className='faq-section'>
+        <div className="faq-section">
           <FeatureCard faq="faq" />
         </div>
+        <div className="order-section">
+          <div>
+            <img src={Message} alt="" />
+            <h3 className="h3">Shipping</h3>
+            <p className="p">Free shpping to all orders in Lagos</p>
+          </div>
+          <div>
+            <img src={Message} alt="" />
+            <h3 className="h3">Shipping</h3>
+            <p className="p">Free shpping to all orders in Lagos</p>
+          </div>
+          <div>
+            <img src={Message} alt="" />
+            <h3 className="h3">Shipping</h3>
+            <p className="p">Free shpping to all orders in Lagos</p>
+          </div>
+        </div>
+        <footer className="home-footer-section">
+          <section className="home-footer-header">
+            <div>
+              <h3 className="h3">New to Our Website?</h3>
+              <p className="p">Get latest Update on new Products and Deals</p>
+            </div>
+            <form className="home-footer-form">
+              <div>
+                <input type="text" placeholder="Email" />
+                <Button title="Subscribe" />
+              </div>
+            </form>
+          </section>
+          <section className="home-footer-main">
+            <div>
+              <img src={footerlogo} alt="" />
+              <h3 className="h3">Lagos</h3>
+              <p className="p">All our products are checked befor shipment</p>
+              <div className="flex">
+                <img src={Visa} alt="" />
+                <img src={Master} alt="" />
+              </div>
+            </div>
+            <ul>
+              <h3 className="h3">Quick Links</h3>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+            </ul>
+            <ul>
+              <h3 className="h3">Quick Links</h3>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+            </ul>
+            <ul>
+              <h3 className="h3">Quick Links</h3>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+              <p className="p">About</p>
+            </ul>
+          </section>
+          <section className="home-footer-footer">
+            <div className="hr"></div>
+            <div className="flex">
+              <div>
+                <CgFacebook />
+              </div>
+              <div>
+                <CgInstagram />
+              </div>
+              <div>
+                <CiTwitter />
+              </div>
+            </div>
+            <p className="p">
+              Copyright @ 2022. All rights reserved. Developed by{' '}
+              <span>Astrosoft</span>
+            </p>
+          </section>
+        </footer>
       </div>
     </>
   )
